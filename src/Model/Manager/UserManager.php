fdc044042486563f443c369e9995b1a2c62a3560
Michael Laau <michael.laau2401@gmail.com>
2026-09-11 01:03:03 +0200

    feat: add User entity and UserManager with findByEmail and create methods

---
+++ src/Model/Manager/UserManager.php
@@ -0,0 +1,41 @@
+<?php 
+
+class UserManager{
+
+    private PDO $db;
+
+    public function __construct(){
+        $this->db = Database::getConnection();
+    }
+
+    public function findByEmail(string $email) : ?User{
+        $stmt = $this->db->prepare('SELECT * FROM users WHERE email = :email');
+        $stmt->execute([
+            'email' => $email
+        ]);
+
+        $data = $stmt->fetch();
+        if(!$data){
+            return null;
+        }
+
+        $user = new User();
+        $user->setId($data['id']);
+        $user->setUsername($data['username']);
+        $user->setEmail($data['email']);
+        $user->setPassword($data['password']);
+        $user->setAvatar($data['avatar']);
+        $user->setCreatedAt(new DateTime($data['created_at']));
+
+        return $user;
+    }
+
+    public function create(User $user) : void{
+        $stmt = $this->db->prepare('INSERT INTO users (username, email, password) VALUES (:username, :email, :password)');
+        $stmt->execute([
+            'username' => $user->getUsername(),
+            'email' => $user->getEmail(),
+            'password' => $user->getPassword()
+        ]);
+    }
+}
