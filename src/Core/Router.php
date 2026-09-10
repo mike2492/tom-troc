5066cde6da2317507b43fd67ab77e5012552f58f
Michael Laau <michael.laau2401@gmail.com>
2026-09-11 00:43:51 +0200

    feat: add basic router with controller/action dispatch

---
+++ src/Core/Router.php
@@ -0,0 +1,28 @@
+<?php
+
+class Router{
+
+    public function run(){
+
+        if(!isset($_GET['controller']) || !isset($_GET['action'])){
+            echo "Paramètres manquants";
+            exit;
+        }
+
+        $controller = $_GET['controller'];
+        $action = $_GET['action'];
+
+        $controllerClass = ucfirst($controller) . 'Controller';
+        if(class_exists($controllerClass)){
+            $controllerInstance = new $controllerClass();
+            if(method_exists($controllerInstance, $action)){
+                $controllerInstance->$action();
+            } else{
+                echo "La méthode n'existe pas";
+            }
+
+        } else{
+            echo "La classe n'existe pas";
+        }
+    }
+}
