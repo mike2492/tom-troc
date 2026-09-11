31d7799cbc68132b0a4f4bfb0063c5c085eae165
Michael Laau <michael.laau2401@gmail.com>
2026-09-11 02:58:21 +0200

    feat: add book detail page with owner lookup

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -1,5 +1,4 @@
 <?php
-// src/Controller/BookController.php
 
 class BookController extends Controller
 {
@@ -15,5 +14,23 @@ class BookController extends Controller
 
         $books = $bookManager->findAll($search);
         $this->render('book/list', ['books' => $books, 'search' => $search]);
+    }
+
+    public function show(): void{
+        $id = (int) $_GET['id'];
+
+        $bookManager = new BookManager();
+
+        $book = $bookManager->findById($id);
+
+        if ($book === null) {
+            header('Location: index.php?controller=book&action=list');
+            exit;
+        }
+
+        $userManager = new UserManager();
+        $owner = $userManager->findById($book->getUserId());
+
+        $this->render('book/show', ['book' => $book, 'owner' => $owner]);
     }
 }
--- src/Model/Manager/BookManager.php
+++ src/Model/Manager/BookManager.php
@@ -34,4 +34,30 @@ class BookManager{
 
         return $books;
     }
+
+    public function findById(int $id) : ?Book{
+        $stmt = $this->db->prepare('SELECT * FROM books WHERE id = :id');
+        $stmt->execute([
+            'id' => $id
+        ]);
+
+        $data = $stmt->fetch();
+
+        if(!$data){
+            return null;
+        }
+        
+        $book = new Book();
+        $book->setId($data['id']);
+        $book->setTitle($data['title']);
+        $book->setAuthor($data['author']);
+        $book->setDescription($data['description']);
+        $book->setImage($data['image']);
+        $book->setAvailable($data['available']);
+        $book->setCreatedAt(new DateTime($data['created_at']));
+        $book->setUserId($data['user_id']);
+
+        return $book;
+        
+    }
 }
