579bf962156e9c3913634ac4486c26159a86c182
Michael Laau <michael.laau2401@gmail.com>
2026-09-26 05:23:16 +0200

    feat: add findLatest method to BookManager

--- src/Model/Manager/BookManager.php
+++ src/Model/Manager/BookManager.php
@@ -138,4 +138,29 @@ class BookManager{
 
         return $books;
     }
+
+    public function findLatest(int $limit) : array{
+        $stmt = $this->db->prepare('SELECT * FROM books ORDER BY created_at DESC LIMIT :limit');
+        $stmt->bindValue('limit', $limit, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $books = [];
+        $rows = $stmt->fetchAll();
+
+        foreach($rows as $row){
+            $book = new Book();
+            $book->setId($row['id']);
+            $book->setTitle($row['title']);
+            $book->setAuthor($row['author']);
+            $book->setDescription($row['description']);
+            $book->setImage($row['image']);
+            $book->setAvailability($row['availability']);
+            $book->setCreatedAt(new DateTime($row['created_at']));
+            $book->setUserId($row['user_id']);
+
+            $books[] = $book;
+        }
+
+        return $books;
+    }
 }
